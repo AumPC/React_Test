3da5f32e0f87acb346b8648e0e68f9dc17f5511a
AumPC <blue-aum@live.com>
2018-11-09 01:10:56 +0700

    Fetch Data StackMethod

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -63,49 +63,52 @@ class Body extends Component {
 		label_y: 'Requests'
 	};
 
-	constructor(props) {
-		super(props);
-	
-		this.state = {
-			hits: [],
-			isLoading: false,
-			error: null,
-			data: [
-				{method: 'POST', amount: 2500},
-				{method: 'GET', amount: 3000}
-			],
-			dataTime: [
-				{
-					data: [
-					{x: 1, y: 300},
-					{x: 2, y: 250},
-					{x: 3, y: 180},
-					{x: 4, y: 200}]
-				},
-				{
-					data: [
-					{x: 1, y: 240},
-					{x: 2, y: 380},
-					{x: 3, y: 300},
-					{x: 4, y: 340}]
-				}
-			],
-			tickValues: [1, 2],
-			label_x: 'Time',
-			label_y: 'Requests'
-		};
-	  }
-
 	componentDidMount() {
 		this.setState({ isLoading: true });
-
+		this.query = {
+			"size": 0,
+			"aggs" : {
+				"req_time_group" : {
+					"date_histogram" : {
+						"field" : "req_time_human",
+						"interval" : "30s"
+					},
+					"aggs" : {
+						"method_group" : {
+							"terms" : { "field" : "method.keyword" }
+						}
+					}
+				}
+			}
+		}
 		axios.get("http://10.3.132.198:9200/web-anon/_search", {
 			params: {
-				size: 10,
-			}})
+				source: JSON.stringify(this.query),
+				source_content_type: 'application/json'
+				
+		}})
 		.then((res) => {
-			console.log(res);
-			this.setState({ isLoading: false });
+			this.datas = {}
+			res['data']['aggregations']['req_time_group']['buckets'].forEach((data) => {
+				if( !(data['key_as_string'] in this.datas) ) {
+					this.datas[data['key_as_string']] = {};
+				} 
+				data['method_group']['buckets'].forEach((type_method) => {
+					this.datas[data['key_as_string']][type_method['key']] = type_method['doc_count'];
+				})
+			})
+			this.dataMethod = { "GET" : [], "POST" : [], "PUT" : [], "DELETE" : [], "HTTPS" : [], "HEAD" : []};
+			for(var key in this.datas) {
+				for(var methods in this.dataMethod) {
+					if( this.datas[key][methods] != undefined ) {
+						this.dataMethod[methods].push({ "date" : key, "count" : this.datas[key][methods] })
+					} else {
+						this.dataMethod[methods].push({ "date" : key, "count" : 0 })
+					}
+				}
+			}
+			console.log("E",this.dataMethod)
+			this.setState({ isLoading: false, dataMethod: this.dataMethod});
 		  })
 		.catch(error => this.setState({
 		  isLoading: false

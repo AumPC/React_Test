973d691ba1e26ca3d877b1c0bbec5f6d808f1750
Thebreak35 <3gthebreak@gmail.com>
2018-11-27 16:19:03 +0700

    add table

--- Frontend/src/components/tables.jsx
+++ Frontend/src/components/tables.jsx
@@ -1,47 +1,57 @@
 import React, {Component} from 'react';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
 
 class Tables extends Component {
-	state = {};
+	constructor() {
+		super();
+		this.state = {
+			data: []
+		}
+	}
 
 	render() {
 		return(
-				<table>
-					<tr>
-						<th>Company</th>
-						<th>Contact</th>
-						<th>Country</th>
-					</tr>
-					<tr>
-						<td>Alfreds Futterkiste</td>
-						<td>Maria Anders</td>
-						<td>Germany</td>
-					</tr>
-					<tr>
-						<td>Centro comercial Moctezuma</td>
-						<td>Francisco Chang</td>
-						<td>Mexico</td>
-					</tr>
-					<tr>
-						<td>Ernst Handel</td>
-						<td>Roland Mendel</td>
-						<td>Austria</td>
-					</tr>
-					<tr>
-						<td>Island Trading</td>
-						<td>Helen Bennett</td>
-						<td>UK</td>
-					</tr>
-					<tr>
-						<td>Laughing Bacchus Winecellars</td>
-						<td>Yoshi Tannamuri</td>
-						<td>Canada</td>
-					</tr>
-					<tr>
-						<td>Magazzini Alimentari Riuniti</td>
-						<td>Giovanni Rovelli</td>
-						<td>Italy</td>
-					</tr>
-				</table>
+				<div>
+					<ReactTable 
+						data={this.state.data}
+						columns={[
+							{
+								Header: "Name",
+								columns: [
+									{
+										Header: "First Name",
+										accessor: "firstname"
+									},
+									{
+										Header: "Last Name",
+										accessor: "lastname"
+									}
+								]
+							},
+							{
+								Header: "Info",
+								columns: [
+									{
+										Header: "Visit",
+										accessor: "visit"
+									},
+									{
+										Header: "Egress",
+										accessor: "egress"
+									},
+									{
+										Header: "Ingress",
+										accessor: "ingress"
+									}
+								]
+							}
+							]}
+							defaultPageSize = {10}
+							className="-striped -highlight"
+					/>
+				</div>
 			);
 	};
 };
